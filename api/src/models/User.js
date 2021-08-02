bf1e44582e40fc9968020b4668d2044eafd82854
Sibyl Perez <sibylperezl@gmail.com>
2021-08-02 14:36:11 -0300

    Backend: Creación modelo User

---
+++ api/src/models/User.js
@@ -0,0 +1,68 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('user', {
+        id:{
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true, 
+        },
+        firstName: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+        },
+        lastName:{
+            type: DataTypes.STRING(255),
+            allowNull: false,
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        ID_Passport:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        address: {
+            type:DataTypes.STRING,
+            allowNull: false,
+        },
+        phoneNumber: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        photo: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        score: {
+            type: DataTypes.FLOAT,
+            allowNull: true,
+        },
+        jobDone: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        isVaccinated:{
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+        },
+        isNew: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+        },
+        isAdmin: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true
+        },
+        isActive: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+        },
+    });
+};
