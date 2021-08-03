d2c5baf4c6053332539618ab5616db7687e32637
Mauro Vera <81174890+Mauro069@users.noreply.github.com>
2021-08-03 19:03:33 -0300

    Update LandingPage.jsx

--- client/src/views/LandingPage/LandingPage.jsx
+++ client/src/views/LandingPage/LandingPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function LandingPage() {
   return (
     <div>
-      <nav ClassName="flex items-center">Soy el nuevo navbar</nav>
+      <nav ClassName="flex items-center">Soy el nuevo navbar X2</nav>
     </div>
   );
 }
