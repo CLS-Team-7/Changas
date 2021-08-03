0739832994fb3c5484f48296c56d06253c359a5b
JAVIER KEVIN OJEDA <mambito1998@gmail.com>
2021-08-03 14:55:56 -0300

    AddComponent(CardPost)

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import CardPost from '../Components/CardPost/CardPost'
 
 function Home() {
     return (
         <div>
             <h1>Changas</h1>
+            <CardPost />
         </div>
     )
 }
