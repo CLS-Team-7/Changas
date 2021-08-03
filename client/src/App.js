21fbbd25235d76d25c177c3724169b516a4a7ae3
Mauro <Maurovera069@gmail.com>
2021-08-03 15:09:40 -0300

    Landing

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@
 import { Route, Switch } from 'react-router-dom';
 import Home from './views/Home'
 import './App.css';
+import LandingPage from './views/LandingPage';
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <Switch>
         <Route path='/home'>
           <Home />
+        </Route>
+        <Route exact path='/'>
+          <LandingPage />
         </Route>
       </Switch>
     </div>
