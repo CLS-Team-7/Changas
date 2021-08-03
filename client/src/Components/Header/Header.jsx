a1cf353da88c0b7412666a3d83835e0b89b19f2b
Dario Gomez <dario_07_11@hotmail.com>
2021-08-03 15:06:26 -0300

    changed buttons

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
                     <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                 </svg>
-                <span className="ml-3 text-xl">Tailblocks</span>
+                <span className="ml-3 text-xl">Changas</span>
                 </a>
                 <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
                 <a className="mr-5 hover:text-gray-900">First Link</a>
@@ -16,7 +16,7 @@ function Header() {
                 <a className="mr-5 hover:text-gray-900">Third Link</a>
                 <a className="mr-5 hover:text-gray-900">Fourth Link</a>
                 </nav>
-                <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
+                <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login / SignUp
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
