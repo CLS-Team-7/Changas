a5c4b0bcb4e74b6ca53e6702058db1f9818f3848
Pablo Gomila <pablogiphone@icloud.com>
2021-08-04 14:46:34 -0300

    added questions

--- client/src/Components/FaqComponent/FaqComponent.jsx
+++ client/src/Components/FaqComponent/FaqComponent.jsx
@@ -7,11 +7,8 @@ function FaqComponen() {
         <div class="container px-5 py-24 mx-auto">
           <div class="text-center mb-20">
             <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">
-              Preguntas frecuentes
+              Preguntas frecuentes de Changas
             </h1>
-            <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-              Algunas de nuestras preguntas frecuentes, aquí las respuestas
-            </p>
           </div>
           <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
             <div class="w-full lg:w-1/2 px-4 py-2">
