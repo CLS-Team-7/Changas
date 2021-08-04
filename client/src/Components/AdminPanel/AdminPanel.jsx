45a8c42fe7fb2d108aef720b74803b09304c67ba
JAVIER KEVIN OJEDA <mambito1998@gmail.com>
2021-08-04 15:08:43 -0300

    Update AdminPanel.jsx

--- client/src/Components/AdminPanel/AdminPanel.jsx
+++ client/src/Components/AdminPanel/AdminPanel.jsx
@@ -86,7 +86,7 @@ function AdminPanel() {
                                         scope="col"
                                         className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                                     >
-                                        Adress
+                                        Address
                                     </th>
                                     <th
                                         scope="col"
