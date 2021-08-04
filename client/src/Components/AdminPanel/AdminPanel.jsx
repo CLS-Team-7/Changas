92c1d47f995dbb6832e86c5655eb2daba709dbfc
JAVIER KEVIN OJEDA <mambito1998@gmail.com>
2021-08-04 15:30:51 -0300

    Update AdminPanel.jsx

--- client/src/Components/AdminPanel/AdminPanel.jsx
+++ client/src/Components/AdminPanel/AdminPanel.jsx
@@ -49,7 +49,7 @@ function AdminPanel() {
     const dispatch = useDispatch()
     const [users, setUsers] = useState([]);
     const [currentUser, setCurrentUser] = useState(1);
-    const userPerPage = 12;
+    const userPerPage = 6;
 
     useEffect(() => {
         setUsers(userList)
@@ -106,38 +106,7 @@ function AdminPanel() {
                                 </tr>
                             </thead>
                             <tbody className="bg-white divide-y divide-gray-200">
-                                {users.map((person) => (
-                                    <tr key={person.id}>
-                                        <td className="px-6 py-4 whitespace-nowrap">
-                                            <div className="flex items-center">
-                                                <div className="flex-shrink-0 h-10 w-10">
-                                                    <img className="h-10 w-10 rounded-full" src={person.photo} alt="" />
-                                                </div>
-                                                <div className="ml-4">
-                                                    <div className="text-sm font-medium text-gray-900">{`${person.firstName} ${person.lastName}`}</div>
-                                                    <div className="text-sm text-gray-500">{person.email}</div>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td className="px-6 py-4 whitespace-nowrap">
-                                            <div className="text-sm text-gray-900">{person.address}</div>
-                                            <div className="text-sm text-gray-500">{person.phoneNumber}</div>
-                                        </td>
-                                        <td className="px-6 py-4 whitespace-nowrap">
-                                            <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                                {
-                                                    person.isActive ? "Active" : "disabled"
-                                                }
-                                            </span>
-                                        </td>
-                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.isAdmin ? "Admin" : "Normal"}</td>
-                                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                                                Edit
-                                            </a>
-                                        </td>
-                                    </tr>
-                                ))}
+                                <AdminPanelComp Users={currentUsers} />
                             </tbody>
                         </table>
                     </div>
