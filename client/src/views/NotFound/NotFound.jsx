9a1643c76952d7abd25729e2c01a51ef39055fad
JAVIER KEVIN OJEDA <mambito1998@gmail.com>
2021-08-04 11:35:23 -0300

    Update NotFound.jsx

--- client/src/views/NotFound/NotFound.jsx
+++ client/src/views/NotFound/NotFound.jsx
@@ -8,7 +8,7 @@ function NotFound() {
     return (
         <div className="NotFound">
             <Header />
-            <div class="bg-white dark:bg-gray-800 h-full NotFoundContent">
+            <div class="bg-white dark:bg-gray-800 h-full NotFoundContent min-h-screen">
                 <div class="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
                     <h2 class="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
                         <span class="block">
