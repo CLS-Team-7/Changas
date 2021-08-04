00450cac423b9b5ae4168ced6f9384aa626fb2b1
JAVIER KEVIN OJEDA <mambito1998@gmail.com>
2021-08-04 15:49:10 -0300

    Update UserProfile.jsx

--- client/src/Components/UserProfile/UserProfile.jsx
+++ client/src/Components/UserProfile/UserProfile.jsx
@@ -22,9 +22,7 @@ function UserProfile() {
                 <div class="shadow-lg rounded-2xl bg-white dark:bg-gray-800 p-4">
                     <div class="flex-row gap-4 flex justify-center items-center">
                         <div class="flex-shrink-0">
-                            <a href="#" class="block relative">
-                                <img alt="profil" src={user.photo} class="mx-auto object-cover rounded-full h-16 w-16 " />
-                            </a>
+                            <img alt="profil" src={user.photo} class="mx-auto object-cover rounded-full h-16 w-16 " />
                         </div>
 
                     </div>
