ab795b906787b07cb1840a1ad9cf4a857d330557
Pablo Gomila <pablogiphone@icloud.com>
2021-08-04 15:03:32 -0300

    FAQ styling

--- client/src/Components/FaqComponent/FaqComponent.jsx
+++ client/src/Components/FaqComponent/FaqComponent.jsx
@@ -13,7 +13,7 @@ function FaqComponen() {
           <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
             <div class="w-full lg:w-1/2 px-4 py-2">
               <details class="mb-4">
-                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                <summary class="font-semibold  bg-Medium__Champagne rounded-md py-2 px-4">
                   ¿Qué es Changas / Quienes somos?
                 </summary>
 
@@ -24,7 +24,7 @@ function FaqComponen() {
                 </span>
               </details>
               <details class="mb-4">
-                <summary class="font-semibold bg-gray-200 rounded-md py-2 px-4">
+                <summary class="font-semibold bg-Medium__Champagne rounded-md py-2 px-4">
                   ¿Cuál es nuestro objetivo / nuestros sueños son?
                 </summary>
 
@@ -35,7 +35,7 @@ function FaqComponen() {
                 </span>
               </details>
               <details class="mb-4">
-                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                <summary class="font-semibold  bg-Medium__Champagne rounded-md py-2 px-4">
                   ¿Dónde me registro?
                 </summary>
 
@@ -48,7 +48,7 @@ function FaqComponen() {
             </div>
             <div class="w-full lg:w-1/2 px-4 py-2">
               <details class="mb-4">
-                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                <summary class="font-semibold  bg-Medium__Champagne rounded-md py-2 px-4">
                   ¿De donde sale la idea del servicio?
                 </summary>
 
@@ -59,7 +59,7 @@ function FaqComponen() {
                 </span>
               </details>
               <details class="mb-4">
-                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                <summary class="font-semibold  bg-Medium__Champagne rounded-md py-2 px-4">
                   ¿Por que utilizar nuestra plataforma de servicios?
                 </summary>
 
@@ -70,8 +70,115 @@ function FaqComponen() {
                 </span>
               </details>
               <details class="mb-4">
-                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                <summary class="font-semibold  bg-Medium__Champagne rounded-md py-2 px-4">
                   ¿Cómo contrato un servicio?
+                </summary>
+
+                <span class="px-4 py-2">
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="text-gray-700">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="text-center mb-20">
+            <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">
+              Preguntas frecuentes para ofrecer servicios
+            </h1>
+          </div>
+          <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+            <div class="w-full lg:w-1/2 px-4 py-2">
+              <details class="mb-4">
+                <summary class="font-semibold  bg-Medium__Champagne rounded-md py-2 px-4">
+                  ¿Qué necesito para registrarme y como lo hago?
+                </summary>
+
+                <span>
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="text-gray-700">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="text-center mb-20">
+            <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">
+              Preguntas frecuentes para contratar servicios
+            </h1>
+          </div>
+          <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+            <div class="w-full lg:w-1/2 px-4 py-2">
+              <details class="mb-4">
+                <summary class="font-semibold  bg-Medium__Champagne rounded-md py-2 px-4">
+                  ¿Qué beneficios tiene adquirir este servicio?
+                </summary>
+
+                <span>
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+              <details class="mb-4">
+                <summary class="font-semibold bg-Medium__Champagne rounded-md py-2 px-4">
+                  ¿Cómo contrato un servicio?
+                </summary>
+
+                <span>
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+              <details class="mb-4">
+                <summary class="font-semibold  bg-Medium__Champagne rounded-md py-2 px-4">
+                  ¿Realmente necesito este servicio?
+                </summary>
+
+                <span>
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+            </div>
+            <div class="w-full lg:w-1/2 px-4 py-2">
+              <details class="mb-4">
+                <summary class="font-semibold  bg-Medium__Champagne rounded-md py-2 px-4">
+                  ¿Cómo funciona?
+                </summary>
+
+                <span class="px-4 py-2">
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+              <details class="mb-4">
+                <summary class="font-semibold  bg-Medium__Champagne rounded-md py-2 px-4">
+                  ¿Tiene garantía?
+                </summary>
+
+                <span class="px-4 py-2">
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+              <details class="mb-4">
+                <summary class="font-semibold  bg-Medium__Champagne rounded-md py-2 px-4">
+                  ¿Qué metodos de pago aceptan?
                 </summary>
 
                 <span class="px-4 py-2">
