9530bb76898fc96d892eba19b0e822166d3ba0e0
JAVIER KEVIN OJEDA <mambito1998@gmail.com>
2021-08-04 16:26:29 -0300

    Update AdminPanelComp.jsx
    Cantidad de post del usuario

--- client/src/Components/AdminPanel/AdminPanelComp.jsx
+++ client/src/Components/AdminPanel/AdminPanelComp.jsx
@@ -29,6 +29,13 @@ function AdminPanelComp({ Users }) {
                             }
                         </span>
                     </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                            {
+                                person.posts.length
+                            }
+                        </span>
+                    </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.isAdmin ? "Admin" : "Normal"}</td>
                     <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                         <a href="#" className="text-indigo-600 hover:text-indigo-900">
