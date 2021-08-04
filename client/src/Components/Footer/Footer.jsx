c546e10b710fb17b4e7324a6956ae6b1574809eb
Pablo Gomila <pablogiphone@icloud.com>
2021-08-04 14:49:34 -0300

    redirected footer to Git

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -16,7 +16,7 @@ function Footer() {
                     />
                 </Link>
                 <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2021 Changas —
-                    <a href="https://twitter.com/knyttneve" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@CLS</a>
+                    <a href="https://github.com/Zven7/PG-Changas" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@CLS</a>
                 </p>
                 <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                     <a class="text-gray-500" href='#'>
