dd67bdd84f02f18119ba18738f3cdd5f7557ec8a
JAVIER KEVIN OJEDA <mambito1998@gmail.com>
2021-08-03 21:51:42 -0300

    Update Pagination.js

--- client/src/Components/Pagination/Pagination.js
+++ client/src/Components/Pagination/Pagination.js
@@ -13,7 +13,7 @@ function Pagination({ ctsPerPage, totalCts, paginate }) {
             <ul className="flex pl-0 rounded list-none flex-wrap">
                 {pageNumbers.map(num => (
                         <a href="/home" key={num} className = 'PaginationLinkButton' onClick={(e) => paginate(num, e)}>
-                            <h5 className="first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-lightBlue-500 bg-white text-lightBlue-500">{num}</h5>
+                            <h5 className="first:ml-0 text-xs font-semibold flex w-10 h-10 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-lightBlue-500 bg-white text-lightBlue-500">{num}</h5>
                         </a>
                 ))}
                 </ul>
