9e8b811e8c8986d89d6becb60ef8b41cca8ed82c
Pablo Gomila <pablogiphone@icloud.com>
2021-08-05 11:44:58 -0300

    updated feedback

--- client/src/Components/FaqComponent/FaqComponent.jsx
+++ client/src/Components/FaqComponent/FaqComponent.jsx
@@ -178,11 +178,11 @@ function FaqComponen() {
         </div>
         <hr />
         <div>
-          <div class="flex items-center min-h-screen bg-gray-50 dark:bg-gray-900">
+          <div class="flex items-center min-h-screen ">
             <div class="container mx-auto">
-              <div class="max-w-md mx-auto my-10 bg-white p-5 rounded-md shadow-sm">
+              <div class="max-w-md mx-auto my-10 bg-white p-5 rounded-lg shadow-sm">
                 <div class="text-center">
-                  <h1 class="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">
+                  <h1 class="my-3 text-3xl font-semibold">
                     Contanos tu experiencia
                   </h1>
                   <p class="text-gray-400 dark:text-gray-400">
