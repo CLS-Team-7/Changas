94f7ed3d730fce2d77964f0c3011ad87f2413a36
Pablo Gomila <pablogiphone@icloud.com>
2021-08-05 10:50:44 -0300

    changed FAQ

--- client/src/Components/FaqComponent/FaqComponent.jsx
+++ client/src/Components/FaqComponent/FaqComponent.jsx
@@ -82,6 +82,10 @@ function FaqComponen() {
             </h2>
           </div>
         </div>
+        <h3 class=" mt-10 mb-10 text-2xl font-bold">
+          CHANGAS "CREANDO OPORTUNIDADES"
+        </h3>
+        <hr />
         <h3 class=" mt-10 mb-5 text-5xl font-bold">
           FAQ para quienes ofrecen servicios
         </h3>
@@ -110,9 +114,68 @@ function FaqComponen() {
             </p>
           </div>
         </div>
-        <h3 class=" mt-10 mb-5 text-2xl font-bold">
-        Así de simple, bienvenido a CHANGAS ya estás listo para comenzar!! Ahora a esperar que te contacten..!!
+        <h3 class=" mt-10 mb-10 text-2xl font-bold">
+          Así de simple, bienvenido a CHANGAS ya estás listo para comenzar!!
+          Ahora a esperar que te contacten..!!
         </h3>
+        <hr />
+        <h3 class=" mt-10 mb-5 text-5xl font-bold">
+          FAQ para quien quiere contratar servicios
+        </h3>
+        <div class="p-2">
+          <div class="bg-white p-4 rounded-lg shadow-lg">
+            <h2 class="text-2xl font-bold mb-2 text-gray-800">
+              ¿Qué beneficios tiene adquirir este servicio?
+            </h2>
+            <p class="text-gray-700">
+              Dentro de los varios beneficios que posees en el sitio web es que
+              ofrecemos contratación directa, sin costos adicionales como
+              comisiones, es GRATIS, además de que contas con la posibilidad de
+              negociar directamente con la persona que te ofrece el servicio,
+              pudiendo así mejorar los plazos de pago y flexibilidad de los
+              mismos.
+            </p>
+          </div>
+        </div>
+        <div class="p-2">
+          <div class="bg-white p-4 rounded-lg shadow-lg">
+            <h2 class="text-2xl font-bold mb-2 text-gray-800">
+              ¿Cómo contrato un servicio?
+            </h2>
+            <p class="text-gray-700">
+              Simple, dirígete a la pantalla principal y presiona el botón de
+              Posteos, en esta sección nos dirigimos a la tarjetas de los
+              trabajos ofrecidos y si no encuentras lo que buscas, puedes
+              filtrar según el tipo y disponibilidad del servicio además de
+              buscar por zonas alrededor de ti.
+            </p>
+          </div>
+        </div>
+        <div class="p-2">
+          <div class="bg-white p-4 rounded-lg shadow-lg">
+            <h2 class="text-2xl font-bold mb-2 text-gray-800">
+              ¿Cómo funciona el servicio?
+            </h2>
+            <p class="text-gray-700">
+              Una vez pactado con la persona encargada de dar el servicio,
+              deberás confirmar a través del sitio web el día pactado y formas
+              de pago para dejar una constante y evitar inconvenientes a futuro.
+              Una vez terminado el servicio te pediremos que califiques el mismo
+              para dejar registrada tu experiencia.
+            </p>
+          </div>
+        </div>
+        <div class="p-2">
+          <div class="bg-white p-4 rounded-lg shadow-lg">
+            <h2 class="text-2xl font-bold mb-2 text-gray-800">
+              ¿Qué métodos de pago aceptan?
+            </h2>
+            <p class="text-gray-700">
+              Todo lo que refiere a métodos de pago queda bajo control de ambas
+              partes en el método de contacto de servicios.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   )
