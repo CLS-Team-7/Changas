8dd13a3b23bcaef0144358e0e156a63309a00450
Pablo Gomila <pablogiphone@icloud.com>
2021-08-05 11:33:21 -0300

    added feedback

--- client/src/Components/FaqComponent/FaqComponent.jsx
+++ client/src/Components/FaqComponent/FaqComponent.jsx
@@ -176,6 +176,124 @@ function FaqComponen() {
             </p>
           </div>
         </div>
+        <hr />
+        <div>
+          <div class="flex items-center min-h-screen bg-gray-50 dark:bg-gray-900">
+            <div class="container mx-auto">
+              <div class="max-w-md mx-auto my-10 bg-white p-5 rounded-md shadow-sm">
+                <div class="text-center">
+                  <h1 class="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">
+                    Contanos tu experiencia
+                  </h1>
+                  <p class="text-gray-400 dark:text-gray-400">
+                    Completa abajo y envianos un mensaje
+                  </p>
+                </div>
+                <div class="m-7">
+                  <form
+                    action="https://api.web3forms.com/submit"
+                    method="POST"
+                    id="form"
+                  >
+                    <input
+                      type="hidden"
+                      name="apikey"
+                      value="9f880591-b81c-4e21-9e48-5b06ff409af0"
+                    />
+                    <input
+                      type="hidden"
+                      name="subject"
+                      value="New Submission from Web3Forms"
+                    />
+                    <input
+                      type="hidden"
+                      name="redirect"
+                      value="https://web3forms.com/success"
+                    />
+
+                    <div class="mb-6">
+                      <label
+                        for="name"
+                        class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                      >
+                        Nombre
+                      </label>
+                      <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Tu nombre aquí"
+                        required="false"
+                        class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                      />
+                    </div>
+                    <div class="mb-6">
+                      <label
+                        for="email"
+                        class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                      >
+                        Email Address
+                      </label>
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="you@company.com"
+                        required
+                        class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                      />
+                    </div>
+                    <div class="mb-6">
+                      <label
+                        for="phone"
+                        class="text-sm text-gray-600 dark:text-gray-400"
+                      >
+                        Phone Number
+                      </label>
+                      <input
+                        type="text"
+                        name="phone"
+                        id="phone"
+                        placeholder="+1 (555) 1234-567"
+                        required
+                        class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                      />
+                    </div>
+                    <div class="mb-6">
+                      <label
+                        for="message"
+                        class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                      >
+                        Your Message
+                      </label>
+
+                      <textarea
+                        rows="5"
+                        name="message"
+                        id="message"
+                        placeholder="Your Message"
+                        class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                        required
+                      ></textarea>
+                    </div>
+                    <div class="mb-6">
+                      <button
+                        type="submit"
+                        class="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none"
+                      >
+                        Send Message
+                      </button>
+                    </div>
+                    <p
+                      class="text-base text-center text-gray-400"
+                      id="result"
+                    ></p>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
