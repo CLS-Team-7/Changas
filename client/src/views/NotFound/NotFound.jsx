0a29ccdae344637f539663a4ad2e3fc30ba0afc7
Mauro Vera <81174890+Mauro069@users.noreply.github.com>
2021-08-05 16:35:59 -0300

    Puse los colores de la paleta

--- client/src/views/NotFound/NotFound.jsx
+++ client/src/views/NotFound/NotFound.jsx
@@ -1,36 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Footer from '../../Components/Footer/Footer'
-import Header from '../../Components/Header/Header'
-import './NotFound.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import Footer from "../../Components/Footer/Footer";
+import Header from "../../Components/Header/Header";
+import "./NotFound.css";
 
 function NotFound() {
   return (
-    <div className="NotFound">
+    <div classNameName="NotFound">
       <Header />
-      <div class="bg-white marginauto dark:bg-gray-800 h-full NotFoundContent min-h-screen">
-        <div class="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
-          <h2 class="text-3xl font-extrabold text-red-600 dark:text-white sm:text-4xl">
-            <span class="block font-extrabold text-red-600">ERROR 404</span>
-            <span class="block text-red-600">No se encontró esa ruta</span>
+      <div className="bg-white marginauto dark:bg-gray-800 h-full NotFoundContent min-h-screen">
+        <div className="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
+          <h2 className="text-3xl font-extrabold text-red-600 dark:text-white sm:text-4xl">
+            <span className="block font-extrabold text-Rich__Black">Error 404</span>
+            <span className="block text-red-600">No se encontró esa ruta</span>
           </h2>
-          <div class="lg:mt-0 lg:flex-shrink-0">
-            <div class="mt-12 inline-flex rounded-md shadow">
-              <Link to="/home">
-                <button
-                  type="button"
-                  class="py-4 px-6  bg-Medium__Champagne focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-                >
-                  Go Home
-                </button>
-              </Link>
+          <div className="lg:mt-0 lg:flex-shrink-0">
+            <div className="mt-5 inline-flex rounded-md">
+            <Link to="/home" className="m-5 ">
+            <button class="transition duration-500 ease-in-out px-10 py-2 transform hover:-translate-y-1 hover:scale-110 rounded-full text-white  bg-Viridian__Green font-bold hover:bg-Blue__Shappire ">
+              Home
+            </button>
+          </Link>
             </div>
           </div>
         </div>
       </div>
-      <Footer class="w-full pin-b" />
+      <Footer className="w-full pin-b" />
     </div>
-  )
+  );
 }
 
-export default NotFound
+export default NotFound;
