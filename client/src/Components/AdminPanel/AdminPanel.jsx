f2eb1572f90ffba1c7283706fde0cb74c237b369
JAVIER KEVIN OJEDA <mambito1998@gmail.com>
2021-08-04 22:06:06 -0300

    Update AdminPanel.jsx

--- client/src/Components/AdminPanel/AdminPanel.jsx
+++ client/src/Components/AdminPanel/AdminPanel.jsx
@@ -71,8 +71,8 @@ function AdminPanel() {
 
     return (
         <div className="flex flex-col">
-            <div className="-my-1 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div /* className="-my-1 overflow-x-auto sm:-mx-6 lg:-mx-8" */>
+                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:pl-8">
                     <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                         <table className="min-w-full divide-y divide-gray-200">
                             <thead className="bg-gray-50">
