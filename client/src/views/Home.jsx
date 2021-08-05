12b461eecc0435bf3dae9e91ad54f0ac556cd9a8
JAVIER KEVIN OJEDA <mambito1998@gmail.com>
2021-08-05 13:30:44 -0300

    Update Home.jsx

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -3,11 +3,13 @@ import CardPost from '../Components/CardPost/CardPost'
 import ContainerPostCard from '../Components/ContainerPostCard/ContainerPostCard'
 import Footer from '../Components/Footer/Footer'
 import Header from '../Components/Header/Header'
+import TestPosts from '../Components/TestPosts/TestPosts'
 
 function Home() {
     return (
         <div className="Home">
             <Header />
+            <TestPosts />
             <ContainerPostCard />
             <Footer />
         </div>
