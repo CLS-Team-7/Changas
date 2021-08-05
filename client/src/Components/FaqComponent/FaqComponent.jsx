12592c89d6094aec0ce1bcb8a5caecc6fd6ea63d
Pablo Gomila <pablogiphone@icloud.com>
2021-08-05 12:07:40 -0300

    updated feedback

--- client/src/Components/FaqComponent/FaqComponent.jsx
+++ client/src/Components/FaqComponent/FaqComponent.jsx
@@ -223,8 +223,11 @@ function FaqComponen() {
                         name="name"
                         id="name"
                         placeholder="Tu nombre aquí"
-                        required="false"
-                        class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                        class="w-full px-3 py-2 placeholder-gray-300 border 
+                        border-gray-300 rounded-md focus:outline-none focus:ring 
+                        focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 
+                        dark:text-white dark:placeholder-gray-500 dark:border-gray-600 
+                        dark:focus:ring-gray-900 dark:focus:border-gray-500"
                       />
                     </div>
                     <div class="mb-6">
@@ -239,11 +242,13 @@ function FaqComponen() {
                         name="email"
                         id="email"
                         placeholder="Tu email aquí"
-                        required="false"
-                        class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                        class="w-full px-3 py-2 placeholder-gray-300 border 
+                        border-gray-300 rounded-md focus:outline-none focus:ring 
+                        focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 
+                        dark:text-white dark:placeholder-gray-500 dark:border-gray-600 
+                        dark:focus:ring-gray-900 dark:focus:border-gray-500"
                       />
                     </div>
-
                     <div class="mb-6">
                       <label
                         for="message"
@@ -251,20 +256,24 @@ function FaqComponen() {
                       >
                         Tu mensaje
                       </label>
-
                       <textarea
                         rows="5"
                         name="message"
                         id="message"
                         placeholder="Tu mensaje aquí"
-                        class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                        required="false"
+                        class="w-full px-3 py-2 placeholder-gray-300 border 
+                        border-gray-300 rounded-md focus:outline-none focus:ring 
+                        focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 
+                        dark:text-white dark:placeholder-gray-500 dark:border-gray-600 
+                        dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                        required
                       ></textarea>
                     </div>
                     <div class="mb-6">
                       <button
                         type="submit"
-                        class="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none"
+                        class="w-full px-3 py-4 text-white bg-indigo-500 rounded-md 
+                        focus:bg-indigo-600 focus:outline-none"
                       >
                         Enviar Mensaje
                       </button>
