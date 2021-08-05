dd68da39d65381de3cb2b180400397f46c077f9c
Pablo Gomila <pablogiphone@icloud.com>
2021-08-05 10:20:43 -0300

    changed FAQ

--- client/src/Components/FaqComponent/FaqComponent.jsx
+++ client/src/Components/FaqComponent/FaqComponent.jsx
@@ -4,29 +4,83 @@ function FaqComponen() {
   return (
     <div className="bg-gradient-to-b from-Medium__Champagne h-screen">
       <div class="p-10">
-        <h3 class=" mb-4 text-xl font-bold">FAQ</h3>
-        <div class="bg-white p-6 rounded-lg shadow-lg">
-          <h2 class="text-2xl font-bold mb-2 text-gray-800">
-            ¿Qué es changas / Quienes somos?
-          </h2>
-          <p class="text-gray-700">
-            Somos una empresa tecnológica que se encarga de conectar personas
-            con diferentes rubros, dividiendo en dos secciones principales,
-            tanto para quienes ofrecen servicios de plomeria, gasista, etc, como
-            también personas que requieran contratar dichos servicios.
-          </p>
+        <div class="p-2">
+          <h3 class=" mb-8 text-xl font-bold">FAQ</h3>
+          <div class="bg-white p-4 rounded-lg shadow-lg">
+            <h2 class="text-2xl font-bold mb-2 text-gray-800">
+              ¿Qué es changas / Quienes somos?
+            </h2>
+            <p class="text-gray-700">
+              Somos una empresa tecnológica que se encarga de conectar personas
+              con diferentes rubros, dividiendo en dos secciones principales,
+              tanto para quienes ofrecen servicios de plomeria, gasista, etc,
+              como también personas que requieran contratar dichos servicios.
+            </p>
+          </div>
         </div>
-        <div class="bg-white p-6 rounded-lg shadow-lg">
-          <h2 class="text-2xl font-bold mb-2 text-gray-800">
-            ¿Cual es nuestro objetivo / Nuestros sueños son?
-          </h2>
-          <p class="text-gray-700">
-            Como empresa, ofrecemos un servicio web para que cada persona pueda
-            publicar o requerir trabajo, según su necesidad. Nuestro objetivo es
-            satisfacer rápidamente la demanda de cada persona poniendo en
-            contacto ambas partes, fomentando así, tanto el trabajo como
-            soluciones.
-          </p>
+        <div class="p-2">
+          <div class="bg-white p-4 rounded-lg shadow-lg">
+            <h2 class="text-2xl font-bold mb-2 text-gray-800">
+              ¿Cual es nuestro objetivo / Nuestros sueños son?
+            </h2>
+            <p class="text-gray-700">
+              Como empresa, ofrecemos un servicio web para que cada persona
+              pueda publicar o requerir trabajo, según su necesidad. Nuestro
+              objetivo es satisfacer rápidamente la demanda de cada persona
+              poniendo en contacto ambas partes, fomentando así, tanto el
+              trabajo como soluciones.
+            </p>
+          </div>
+        </div>
+        <div class="p-2">
+          <div class="bg-white p-4 rounded-lg shadow-lg">
+            <h2 class="text-2xl font-bold mb-2 text-gray-800">
+              ¿Dónde me registro?
+            </h2>
+            <p class="text-gray-700">
+              Dirígete a nuestra barra inicial en el botón que dice REGISTRATE;
+              ahí te llevará hacia otra página donde llenarán un formulario con
+              los datos requeridos según tu tipo de perfil.
+            </p>
+          </div>
+        </div>
+        <div class="p-2">
+          <div class="bg-white p-4 rounded-lg shadow-lg">
+            <h2 class="text-2xl font-bold mb-2 text-gray-800">
+              ¿De donde nace la idea del servicio?
+            </h2>
+            <p class="text-gray-700">
+              La idea principal nace en querer ayudar a quienes perdieron sus
+              trabajos a causa de la pandemia, proporcionándoles la posibilidad
+              de tener contratación directa con clientes mediante nuestro sitio
+              web.
+            </p>
+          </div>
+        </div>
+        <div class="p-2">
+          <div class="bg-white p-4 rounded-lg shadow-lg">
+            <h2 class="text-2xl font-bold mb-2 text-gray-800">
+              ¿Por qué utilizar nuestra plataforma de servicios?
+            </h2>
+            <p class="text-gray-700">
+              Poseemos un método de búsquedas por GPS y rango de distancias que
+              te proporcionará contratar de forma muy rápida el servicio que
+              desees, como así también cuenta con una sección de favoritos para
+              calificar el servicio y volver a contactar las mismas personas.
+            </p>
+          </div>
+        </div>
+        <div class="p-2">
+          <div class="bg-white p-4 rounded-lg shadow-lg">
+            <h2 class="text-l font-bold mb-2 text-gray-800">
+              Estos son algunos de los aspectos de nuestro servicio, pero de
+              seguro te preguntas qué costo te saldrá adicional a la
+              contratación, para la sorpresa de todos y como en CHANGAS sabemos
+              el difícil momento que atraviesas decidimos que sea TOTALMENTE
+              GRATIS, así es GRATIS solo te pedimos que llenes la sección de
+              feedback para poder seguir mejorando tu experiencia!!
+            </h2>
+          </div>
         </div>
       </div>
     </div>
