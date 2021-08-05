e1b3f677c0f25412bae098c3a5f2240af5207854
JAVIER KEVIN OJEDA <mambito1998@gmail.com>
2021-08-05 15:48:18 -0300

    Update AdminPanelComp.jsx

--- client/src/Components/AdminPanel/AdminPanelComp.jsx
+++ client/src/Components/AdminPanel/AdminPanelComp.jsx
@@ -4,20 +4,29 @@ import { Link } from 'react-router-dom'
 
 function AdminPanelComp({ Users }) {
     return (
-        < div>
+        <div>
             {Users.map(person => {
-                return <tr key={person.id}>
+                return <tr key={person.id} className="">
                     <td className="px-6 py-4 whitespace-nowrap py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8 " >
                         <div className="flex items-center">
+
                             <div className="flex-shrink-0 h-10 w-10">
+
                                 <img className="h-10 w-10 rounded-full" src={person.photo} alt="" />
                             </div>
                             <div className="ml-4">
+                                <th
+                                    scope="col"
+                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                >
+                                    Name
+                                </th>
                                 <Link to={`/profile/${person.id}`}><div className="text-sm font-medium text-gray-900">{`${person.firstName} ${person.lastName}`}</div></Link>
                                 <div className="text-sm text-gray-500">{person.email}</div>
                             </div>
                         </div>
                     </td>
+
                     <td className="px-6 py-4 whitespace-nowrap">
                         <div className="text-sm text-gray-900">{person.address}</div>
                         <div className="text-sm text-gray-500">{person.phoneNumber}</div>
@@ -30,6 +39,7 @@ function AdminPanelComp({ Users }) {
                         </span>
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap">
+
                         <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
                             {
                                 person.posts.length
