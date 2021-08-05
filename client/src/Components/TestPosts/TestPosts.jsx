f63f8df82b1653f4b5e8b2a76ce6ecb9e6bc6d31
JAVIER KEVIN OJEDA <mambito1998@gmail.com>
2021-08-05 11:55:45 -0300

    Creando test de Menu de usuario

--- client/src/Components/TestPosts/TestPosts.jsx
+++ client/src/Components/TestPosts/TestPosts.jsx
@@ -1,43 +1,12 @@
-import React, {useState, useEffect} from 'react';
-import { getAllPosts } from '../../Redux/actions';
-import { useSelector, useDispatch} from 'react-redux';
-import Pagination from '../Pagination/Pagination'
-import PostsTestComp from './PostsTestComp'
+
 
 function TestPosts() {
-    const dispatch = useDispatch();
-    const [posts, setPosts] = useState([]);
-    const [currentPost, setCurrentPost] = useState(1);
-    const postsPerPage = 12;
-
-
-    const postList = useSelector(state => state.postList)
-
-    useEffect(() => {
-        setPosts(postList)
-    }, [postList])
-
-    useEffect(() => {
-        dispatch(getAllPosts())
-    }, [dispatch])
-
-
-    //Pagination
-    const paginate = (pageNumber, e) => {
-        e.preventDefault();
-        setCurrentPost(pageNumber);
-    }
-
-    const indexOfLastPost = currentPost * postsPerPage;
-    const indexOfFirstPost = indexOfLastPost - postsPerPage;
-    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
 
 
     return (
         <div>
-            <PostsTestComp posts={currentPosts} />
-            <Pagination ctsPerPage={postsPerPage} totalCts={posts.length} paginate={paginate}/>
+
         </div>
     )
 }
