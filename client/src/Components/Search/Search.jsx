ef60b5ca613af83e9d65a2bb267bc73cf56719c4
Pablo Gomila <pablogiphone@icloud.com>
2021-08-06 12:03:19 -0300

    Update Search.jsx

--- client/src/Components/Search/Search.jsx
+++ client/src/Components/Search/Search.jsx
@@ -8,7 +8,7 @@ function Search() {
           class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
           type="search"
           name="search"
-          placeholder="Search"
+          placeholder="Busqueda"
         />
         <button type="submit" class="absolute right-0 top-0 mt-5 mr-4">
           <svg
@@ -18,7 +18,6 @@ function Search() {
             x="0px"
             y="0px"
             viewBox="0 0 56.966 56.966"
-
             width="512px"
             height="512px"
           >
