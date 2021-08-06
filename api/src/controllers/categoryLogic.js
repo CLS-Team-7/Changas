661505172af5b59b1ab9be9d1741e239db9a4b22
Sibyl Perez <sibylperezl@gmail.com>
2021-08-06 17:45:19 -0300

    Refactorización controllers - routes

--- api/src/controllers/categoryLogic.js
+++ api/src/controllers/categoryLogic.js
@@ -2,7 +2,111 @@ const { Op } = require("sequelize")
 const axios = require('axios').default;
 const { User, Post, Order, Category, Specialty } = require('../db.js');
 
+async function getAllCategories (_req, res, next) { //http://localhost:3001/category --> 
+	try {
+		let categories = await Category.findAll({
+			attributes: ["id", "title"],
+			include: {
+				model: Specialty,
+				attributes: ["id", "title"]
+			}
+		});
+		res.json(categories);
+	} catch (err) {
+		next(err);
+	};
+};
 
+
+async function getCategoryById (req, res, next) {
+	let { idCategory } = req.params;
+	if (idCategory && !isNaN(idCategory) && parseInt(idCategory) > 0) { // chequea que el id sea exista, sea un numero, y sea mayor a 0
+		try {
+			let result = await Category.findOne({
+				where: {
+					id: idCategory
+				},
+				attributes: ["id", "title"],
+				include: {
+					model: Specialty,
+					attributes: ["id", "title"]
+				}
+			});
+			if (result) res.json(result);
+			else throw new Error('ERROR 500: La categoria/rubro buscado no fue encontrado en la base de datos (ID no existe)');
+		} catch (err) {
+			next(err);
+		};
+	} else {
+		try {
+			throw new TypeError('ERROR 404: ID inválido (ID no es un tipo INTEGER válido).') // automaticamente rechaza un error, sin buscar por la DB
+		} catch (err) {
+			next(err);
+		}
+	}
+};
+
+
+async function createCategory (req, res, next) {
+	let { title } = req.body;
+	try {
+		let newCategory = await Category.create({
+			title
+		});
+		res.json(newCategory);
+	} catch (err) {
+		next(err);
+	};
+};
+
+async function updateCategory (req, res, next) {
+	let { idCategory } = req.params;
+	let changes = req.body;
+	try {
+		await Category.update(changes, {
+			where: {
+				id: idCategory
+			}
+		});
+		let updatedCategory = await Category.findOne({
+			where: {
+				id: idCategory
+			},
+			// attributes: ["id", "title"],
+			// include: {
+			// 	model: Specialty,
+			// 	attributes: ["id", "title"]
+			// }
+		});
+		res.json(updatedCategory);
+	} catch (err) {
+		next(err);
+	};
+};
+
+async function deleteCategory (req, res, next) {
+	let { idCategory } = req.params;
+	try {
+		let existsInDB = await Category.findByPk(idCategory);
+		if (existsInDB) {
+			await Category.destroy({
+				where: {
+					id: idCategory
+				},
+				// attributes: ["id", "title"],
+				// include: {
+				// 	model: Specialty,
+				// 	attributes: ["id", "title"]
+				// }
+			});
+			return res.json(existsInDB);
+		} else {
+			throw new Error('ERROR 500: La cateogría/rubro no fue encontrado en la base de datos (ID no existe).');
+		}
+	} catch (err) {
+		next(err);
+	};
+};
 
 
 
@@ -11,9 +115,9 @@ const { User, Post, Order, Category, Specialty } = require('../db.js');
 
 
 module.exports = {
-	getAllsarasa,
-	getSarasaById,
-	createSarasa,
-	updateSarasa,
-	deleteSarasa
+	getAllCategories,
+	getCategoryById,
+	createCategory,
+	updateCategory,
+	deleteCategory
 };
--- api/src/controllers/userLogic.js
+++ api/src/controllers/userLogic.js
@@ -2,18 +2,175 @@ const { Op } = require("sequelize")
 const axios = require('axios').default;
 const { User, Post, Order, Category, Specialty } = require('../db.js');
 
+async function getAllUser(_req, res, next) { //http://localhost:3001/user --> 
+	try {
+		let users = await User.findAll({
+			include: {
+				model: Post,
+				attributes: { exclude: ["user_id", "category_id", "specialty_id"] },
+				include: [
+					{
+						model: Category,
+						attributes: ["id", "title"]
+					},
+					{
+						model: Specialty,
+						attributes: ["id", "title"]
+					}
+				]
+			}
+		});
+		res.json(users);
+	} catch (err) {
+		next(err);
+	};
+};
+
+async function getUserById (req, res, next) {
+	let { idUser } = req.params;
+	if (idUser && idUser.length === 36) { // 36 es la length del UUID
+		try {
+			let result = await User.findOne({
+				where: {
+					id: idUser
+				},
+				include: {
+					model: Post,
+					attributes: { exclude: ["user_id", "category_id", "specialty_id"] },
+					include: [
+						{
+							model: Category,
+							attributes: ["id", "title"]
+						},
+						{
+							model: Specialty,
+							attributes: ["id", "title"]
+						}
+					]
+				}
+			});
+			if (result) res.json(result);
+			else throw new Error('ERROR 500: El usuario no fue encontrado en la base de datos (UUID no existe).');
+		} catch (err) {
+			next(err);
+		};
+	};
+	if (idUser && idUser.length !== 36) {
+		try {
+			throw new TypeError('ERROR 404: ID inválido (ID no es un tipo UUID válido).')
+		} catch (err) {
+			next(err);
+		}
+	};
+};
 
 
+async function getFavUsers (req, res, next) {
+	// a desarrollar para demo 1?
+};
 
+async function getMyPosts (req, res, next) {
+	// a desarrollar para demo 1?
+};
 
+async function createUser (req, res, next) {
+	let { firstName, lastName, age, ID_Passport, address, phoneNumber, email, summary, photo, score, jobsDone, isVaccinated, isNew, isAdmin, isActive } = req.body;
+	try {
+		let newUser = await User.create({
+			firstName,
+			lastName,
+			age,
+			ID_Passport,
+			address,
+			phoneNumber,
+			email,
+			summary,
+			photo,
+			score,
+			jobsDone,
+			isVaccinated,
+			isNew,
+			isAdmin,
+			isActive
+		});
+		res.json(newUser);
+	} catch (err) {
+		next(err);
+	};
+};
 
+async function updateUser (req, res, next) {
+	let { idUser } = req.params;
+	let changes = req.body;
+	try {
+		await User.update(changes, {
+			where: {
+				id: idUser
+			}
+		});
+		let updatedUser = await User.findOne({ // await User.findByPk(idUser);
+			where: {
+				id: idUser
+			},
+			// include: {
+			// 	model: Post,
+			// 	attributes: { exclude: ["user_id", "category_id", "specialty_id"] },
+			// 	include: [
+			// 		{
+			// 			model: Category,
+			// 			attributes: ["id", "title"]
+			// 		},
+			// 		{
+			// 			model: Specialty,
+			// 			attributes: ["id", "title"]
+			// 		}
+			// 	]
+			// }
+		});
+		res.json(updatedUser); // se envia el user modificado al front
+	} catch (err) {
+		next(err);
+	};
+};
 
+async function deleteUser (req, res, next) {
+	let { idUser } = req.params;
+	try {
+		let existsInDB = await User.findByPk(idUser); // primero busca si existe el user en la DB. Si existe lo guarda en esta variable 
+		if (existsInDB) {
+			await User.destroy({ // de existir, lo destruye
+				where: {
+					id: idUser
+				},
+				// include: {
+				// 	model: Post,
+				// 	attributes: { exclude: ["user_id"], "category_id", "specialty_id" },
+				// 	include: [
+				// 		{
+				// 			model: Category,
+				// 			attributes: ["id", "title"]
+				// 		},
+				// 		{
+				// 			model: Specialty,
+				// 			attributes: ["id", "title"]
+				// 		}
+				// 	]
+				// }
+			});
+			return res.json(existsInDB); // devuelve el post eliminado como el metodo pop()
+		} else {
+			throw new Error('ERROR 500: El usuario no fue encontrado en la base de datos (UUID no existe).');
+		}
+	} catch (err) {
+		next(err);
+	};
+};
 
 
 module.exports = {
-	getAllsarasa,
-	getSarasaById,
-	createSarasa,
-	updateSarasa,
-	deleteSarasa
+	getAllUser,
+	getUserById,
+	createUser,
+	updateUser,
+	deleteUser
 };
