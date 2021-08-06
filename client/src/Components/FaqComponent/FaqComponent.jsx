02556f594dc0596cc9b10b231dc082525029c47b
Pablo Gomila <pablogiphone@icloud.com>
2021-08-06 09:38:54 -0300

    FAQ Accordion

--- client/src/Components/FaqComponent/FaqComponent.jsx
+++ client/src/Components/FaqComponent/FaqComponent.jsx
@@ -1,295 +1,89 @@
 import React from 'react'
+import FaqComponent from './FaqComponent.css'
 
 function FaqComponen() {
   return (
-    <div className="bg-gradient-to-b from-Medium__Champagne  ">
-      <div class="p-10">
-        <div class="p-2">
-          <h3 class=" mb-5 text-5xl font-bold">FAQ</h3>
-          <div class="bg-white p-4 rounded-lg shadow-lg">
-            <h2 class="text-2xl font-bold mb-2 text-gray-800">
-              ¿Qué es changas / Quienes somos?
-            </h2>
-            <p class="text-gray-700">
-              Somos una empresa tecnológica que se encarga de conectar personas
-              con diferentes rubros, dividiendo en dos secciones principales,
-              tanto para quienes ofrecen servicios de plomeria, gasista, etc,
-              como también personas que requieran contratar dichos servicios.
-            </p>
-          </div>
-        </div>
-        <div class="p-2">
-          <div class="bg-white p-4 rounded-lg shadow-lg">
-            <h2 class="text-2xl font-bold mb-2 text-gray-800">
-              ¿Cual es nuestro objetivo / Nuestros sueños son?
-            </h2>
-            <p class="text-gray-700">
-              Como empresa, ofrecemos un servicio web para que cada persona
-              pueda publicar o requerir trabajo, según su necesidad. Nuestro
-              objetivo es satisfacer rápidamente la demanda de cada persona
-              poniendo en contacto ambas partes, fomentando así, tanto el
-              trabajo como soluciones.
-            </p>
-          </div>
-        </div>
-        <div class="p-2">
-          <div class="bg-white p-4 rounded-lg shadow-lg">
-            <h2 class="text-2xl font-bold mb-2 text-gray-800">
-              ¿Dónde me registro?
-            </h2>
-            <p class="text-gray-700">
-              Dirígete a nuestra barra inicial en el botón que dice REGISTRATE;
-              ahí te llevará hacia otra página donde llenarán un formulario con
-              los datos requeridos según tu tipo de perfil.
-            </p>
-          </div>
-        </div>
-        <div class="p-2">
-          <div class="bg-white p-4 rounded-lg shadow-lg">
-            <h2 class="text-2xl font-bold mb-2 text-gray-800">
-              ¿De donde nace la idea del servicio?
-            </h2>
-            <p class="text-gray-700">
-              La idea principal nace en querer ayudar a quienes perdieron sus
-              trabajos a causa de la pandemia, proporcionándoles la posibilidad
-              de tener contratación directa con clientes mediante nuestro sitio
-              web.
-            </p>
-          </div>
-        </div>
-        <div class="p-2">
-          <div class="bg-white p-4 rounded-lg shadow-lg">
-            <h2 class="text-2xl font-bold mb-2 text-gray-800">
-              ¿Por qué utilizar nuestra plataforma de servicios?
-            </h2>
-            <p class="text-gray-700">
-              Poseemos un método de búsquedas por GPS y rango de distancias que
-              te proporcionará contratar de forma muy rápida el servicio que
-              desees, como así también cuenta con una sección de favoritos para
-              calificar el servicio y volver a contactar las mismas personas.
-            </p>
-          </div>
-        </div>
-        <div class="p-2">
-          <div class="bg-white p-4 rounded-lg shadow-lg">
-            <h2 class="text-l font-bold mb-2 text-gray-800">
-              Estos son algunos de los aspectos de nuestro servicio, pero de
-              seguro te preguntas qué costo te saldrá adicional a la
-              contratación, para la sorpresa de todos y como en CHANGAS sabemos
-              el difícil momento que atraviesas decidimos que sea TOTALMENTE
-              GRATIS, así es GRATIS solo te pedimos que llenes la sección de
-              feedback para poder seguir mejorando tu experiencia!!
-            </h2>
-          </div>
-        </div>
-        <h3 class=" mt-10 mb-10 text-2xl font-bold">
-          CHANGAS "CREANDO OPORTUNIDADES"
-        </h3>
-        <hr />
-        <h3 class=" mt-10 mb-5 text-5xl font-bold">
-          FAQ para quienes ofrecen servicios
-        </h3>
-        <div class="p-2">
-          <div class="bg-white p-4 rounded-lg shadow-lg">
-            <h2 class="text-2xl font-bold mb-2 text-gray-800">
-              ¿Qué necesito para registrarme?
-            </h2>
-            <p class="text-gray-700">
-              Para ingresar deberás registrarte con tu correo electrónico y
-              llenar el formulario correspondiente a tu tipo de cuenta en las
-              diferentes secciones. Debes ser mayor de 18 años y mantener los
-              protocolos de higiene pactados en el formulario.
-            </p>
-          </div>
-        </div>
-        <div class="p-2">
-          <div class="bg-white p-4 rounded-lg shadow-lg">
-            <h2 class="text-2xl font-bold mb-2 text-gray-800">
-              ¿Cómo publico mi servicio?
-            </h2>
-            <p class="text-gray-700">
-              Dirígete a la sección de post, ahora ve al botón crear post,
-              perfecto, ahora necesitas llenar todos los campos para tener un
-              posteo exitoso!!
-            </p>
-          </div>
-        </div>
-        <h3 class=" mt-10 mb-10 text-2xl font-bold">
-          Así de simple, bienvenido a CHANGAS ya estás listo para comenzar!!
-          Ahora a esperar que te contacten..!!
-        </h3>
-        <hr />
-        <h3 class=" mt-10 mb-5 text-5xl font-bold">
-          FAQ para quien quiere contratar servicios
-        </h3>
-        <div class="p-2">
-          <div class="bg-white p-4 rounded-lg shadow-lg">
-            <h2 class="text-2xl font-bold mb-2 text-gray-800">
-              ¿Qué beneficios tiene adquirir este servicio?
-            </h2>
-            <p class="text-gray-700">
-              Dentro de los varios beneficios que posees en el sitio web es que
-              ofrecemos contratación directa, sin costos adicionales como
-              comisiones, es GRATIS, además de que contas con la posibilidad de
-              negociar directamente con la persona que te ofrece el servicio,
-              pudiendo así mejorar los plazos de pago y flexibilidad de los
-              mismos.
-            </p>
-          </div>
-        </div>
-        <div class="p-2">
-          <div class="bg-white p-4 rounded-lg shadow-lg">
-            <h2 class="text-2xl font-bold mb-2 text-gray-800">
-              ¿Cómo contrato un servicio?
-            </h2>
-            <p class="text-gray-700">
-              Simple, dirígete a la pantalla principal y presiona el botón de
-              Posteos, en esta sección nos dirigimos a la tarjetas de los
-              trabajos ofrecidos y si no encuentras lo que buscas, puedes
-              filtrar según el tipo y disponibilidad del servicio además de
-              buscar por zonas alrededor de ti.
-            </p>
-          </div>
-        </div>
-        <div class="p-2">
-          <div class="bg-white p-4 rounded-lg shadow-lg">
-            <h2 class="text-2xl font-bold mb-2 text-gray-800">
-              ¿Cómo funciona el servicio?
-            </h2>
-            <p class="text-gray-700">
-              Una vez pactado con la persona encargada de dar el servicio,
-              deberás confirmar a través del sitio web el día pactado y formas
-              de pago para dejar una constante y evitar inconvenientes a futuro.
-              Una vez terminado el servicio te pediremos que califiques el mismo
-              para dejar registrada tu experiencia.
-            </p>
-          </div>
-        </div>
-        <div class="p-2 pb-10">
-          <div class="bg-white p-4 rounded-lg shadow-lg">
-            <h2 class="text-2xl font-bold mb-2 text-gray-800">
-              ¿Qué métodos de pago aceptan?
-            </h2>
-            <p class="text-gray-700">
-              Todo lo que refiere a métodos de pago queda bajo control de ambas
-              partes en el método de contacto de servicios.
-            </p>
-          </div>
-        </div>
-        <hr />
-        <div>
-          <div class="flex items-center ">
-            <div class="container mx-auto">
-              <div class="max-w-md mx-auto my-10 bg-white p-5 rounded-lg shadow-lg">
-                <div class="text-center">
-                  <h1 class="my-3 text-3xl font-semibold">
-                    Contanos tu experiencia
-                  </h1>
-                  <p class="text-gray-400 dark:text-gray-400">
-                    Completa abajo y envianos un mensaje
-                  </p>
-                </div>
-                <div class="m-7">
-                  <form
-                    action="https://api.web3forms.com/submit"
-                    method="POST"
-                    id="form"
-                  >
-                    <input
-                      type="hidden"
-                      name="apikey"
-                      value="9f880591-b81c-4e21-9e48-5b06ff409af0"
-                    />
-                    <input
-                      type="hidden"
-                      name="subject"
-                      value="New Submission from Web3Forms"
-                    />
-                    <input
-                      type="hidden"
-                      name="redirect"
-                      value="https://web3forms.com/success"
-                    />
-
-                    <div class="mb-6">
-                      <label
-                        for="name"
-                        class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
-                      >
-                        Nombre
-                      </label>
-                      <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        placeholder="Tu nombre aquí"
-                        class="w-full px-3 py-2 placeholder-gray-300 border 
-                        border-gray-300 rounded-md focus:outline-none focus:ring 
-                        focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 
-                        dark:text-white dark:placeholder-gray-500 dark:border-gray-600 
-                        dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                      />
-                    </div>
-                    <div class="mb-6">
-                      <label
-                        for="email"
-                        class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
-                      >
-                        Dirección de Email
-                      </label>
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        placeholder="Tu email aquí"
-                        class="w-full px-3 py-2 placeholder-gray-300 border 
-                        border-gray-300 rounded-md focus:outline-none focus:ring 
-                        focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 
-                        dark:text-white dark:placeholder-gray-500 dark:border-gray-600 
-                        dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                      />
-                    </div>
-                    <div class="mb-6">
-                      <label
-                        for="message"
-                        class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
-                      >
-                        Tu mensaje
-                      </label>
-                      <textarea
-                        rows="5"
-                        name="message"
-                        id="message"
-                        placeholder="Tu mensaje aquí"
-                        class="w-full px-3 py-2 placeholder-gray-300 border 
-                        border-gray-300 rounded-md focus:outline-none focus:ring 
-                        focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 
-                        dark:text-white dark:placeholder-gray-500 dark:border-gray-600 
-                        dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                        required
-                      ></textarea>
-                    </div>
-                    <div class="mb-6">
-                      <button
-                        type="submit"
-                        class="w-full px-3 py-4 text-white bg-indigo-500 rounded-md 
-                        focus:bg-indigo-600 focus:outline-none"
-                      >
-                        Enviar Mensaje
-                      </button>
-                    </div>
-                    <p
-                      class="text-base text-center text-gray-400"
-                      id="result"
-                    ></p>
-                  </form>
-                </div>
-              </div>
+    <>
+      <div class="container mx-auto">
+      <div class="m-8 rounded overflow-hidden">
+ 
+        <div class="group outline-none accordion-section" tabindex="1">
+          <div
+            class="group bg-gray-900 flex justify-between px-4 py-3 items-center text-gray-500 transition ease duration-500 cursor-pointer pr-10 relative"
+          >
+            <div class="group-focus:text-white transition ease duration-500">
+              Title for Tab - 1
+            </div>
+            <div
+              class="h-8 w-8 border border-gray-700 rounded-full items-center inline-flex justify-center transform transition ease duration-500 group-focus:text-white group-focus:-rotate-180 absolute top-0 right-0 mb-auto ml-auto mt-2 mr-2"
+            >
+              <i class="fas fa-chevron-down"></i>
             </div>
           </div>
+          <div
+            class="group-focus:max-h-screen max-h-0 bg-gray-800 px-4 overflow-hidden ease duration-500"
+          >
+            <p class="p-2 text-gray-400 text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              repellat amet doloribus consequuntur eos similique provident
+              tempora voluptates iure quia fuga dicta voluptatibus culpa
+              mollitia recusandae delectus id suscipit labore?
+            </p>
+          </div>
         </div>
-      </div>
-    </div>
+
+        <div class="group outline-none accordion-section" tabindex="2">
+          <div
+            class="group bg-gray-900 flex justify-between px-4 py-3 items-center text-gray-500 transition ease duration-500 cursor-pointer pr-10 relative"
+          >
+            <div class="group-focus:text-white transition ease duration-500">
+              Title for Tab - 2
+            </div>
+            <div
+              class="h-8 w-8 border border-gray-700 rounded-full items-center inline-flex justify-center transform transition ease duration-500 group-focus:text-white group-focus:-rotate-180 absolute top-0 right-0 mb-auto ml-auto mt-2 mr-2"
+            >
+              <i class="fas fa-chevron-down"></i>
+            </div>
+          </div>
+          <div
+            class="group-focus:max-h-screen max-h-0 bg-gray-800 px-4 overflow-hidden ease duration-500"
+          >
+            <p class="p-2 text-gray-400 text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              repellat amet doloribus consequuntur eos similique provident
+              tempora voluptates iure quia fuga dicta voluptatibus culpa
+              mollitia recusandae delectus id suscipit labore?
+            </p>
+          </div>
+        </div>
+    
+        <div class="group outline-none accordion-section" tabindex="3">
+          <div
+            class="group bg-gray-900 flex justify-between px-4 py-3 items-center text-gray-500 transition ease duration-500 cursor-pointer pr-10 relative"
+          >
+            <div class="group-focus:text-white transition ease duration-500">
+              Title for Tab - 3
+            </div>
+            <div
+              class="h-8 w-8 border border-gray-700 rounded-full items-center inline-flex justify-center transform transition ease duration-500 group-focus:text-white group-focus:-rotate-180 absolute top-0 right-0 mb-auto ml-auto mt-2 mr-2"
+            >
+              <i class="fas fa-chevron-down"></i>
+            </div>
+          </div>
+          <div
+            class="group-focus:max-h-screen max-h-0 bg-gray-800 px-4 overflow-hidden ease duration-500"
+          >
+            <p class="p-2 text-gray-400 text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              repellat amet doloribus consequuntur eos similique provident
+              tempora voluptates iure quia fuga dicta voluptatibus culpa
+              mollitia recusandae delectus id suscipit labore?
+            </p>
+          </div>
+        </div>
+        </div>
+        </div>
+    </>
   )
 }
 
