ffc313f3a24612f00f9c5e6927fe66670ceda4e3
JAVIER KEVIN OJEDA <mambito1998@gmail.com>
2021-08-07 18:26:13 -0300

    Update PostComp.jsx

--- client/src/Components/ContainerPostCard/PostComp.jsx
+++ client/src/Components/ContainerPostCard/PostComp.jsx
@@ -11,6 +11,7 @@ function PostComp({ posts }) {
                     img={post.image}
                     category={post.category}
                     description={post.description}
+                    specialty={post.specialty}
                     id={post.id}
                     key={post.id} />
             })}
