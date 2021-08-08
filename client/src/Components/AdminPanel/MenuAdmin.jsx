3f08102bf503ec7887fea665b39a2a7143902b6d
Dario Gomez <dario_07_11@hotmail.com>
2021-08-08 15:04:47 -0300

    Added Category Counter in Admin Panel

--- client/src/Components/AdminPanel/MenuAdmin.jsx
+++ client/src/Components/AdminPanel/MenuAdmin.jsx
@@ -1,11 +1,19 @@
 import { useAuth0 } from '@auth0/auth0-react'
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getAllCategory } from '../../Redux/actions'
 import AdminPanel from './AdminPanel'
 import SideBarAdmin from './SideBarAdmin/SideBarAdmin'
 
 function MenuAdmin() {
-    const userList = useSelector(state => state.userAdmin)
+    const userList = useSelector(state => state.userAdmin);
+    const categoryList = useSelector(state => state.categoryPost);
+    const dispatch = useDispatch();
+
+    useEffect(state => {
+        dispatch(getAllCategory());
+    }, [dispatch])
+
     const { user } = useAuth0()
     return (
         <main className="bg-gray-100 dark:bg-gray-800 h-screen overflow-hidden  lg:flex-col pt:2">
@@ -30,6 +38,20 @@ function MenuAdmin() {
                                         </p>
                                         <p className="text-gray-400 text-sm">
                                             Cuentas Creadas
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex my-6 items-center w-full space-y-4 md:space-x-4 md:space-y-0 flex-col md:flex-row">
+                            <div className="flex items-center w-full md:w-1/2 space-x-4">
+                                <div className="w-1/2">
+                                    <div className="shadow-lg px-4 py-6 w-full bg-white dark:bg-gray-700 relative">
+                                        <p className="text-2xl text-black dark:text-white font-bold">
+                                            {categoryList.length}
+                                        </p>
+                                        <p className="text-gray-400 text-sm">
+                                            Categorias Creadas
                                         </p>
                                     </div>
                                 </div>
