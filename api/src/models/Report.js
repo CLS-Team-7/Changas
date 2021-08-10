dba6f82835df61a6ab9be06bb4ff24394bf01c2b
Pablo Gomila <pablogiphone@icloud.com>
2021-08-10 14:05:50 -0300

    modified models

--- api/src/models/Report.js
+++ api/src/models/Report.js
@@ -1,17 +1,24 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require('sequelize')
 
 module.exports = (sequelize) => {
-	sequelize.define('complaint', {
-		id: {
-			type: DataTypes.INTEGER,
-			autoIncrement: true,
-			allowNull: false,
-			primaryKey: true
-		},
-		title: {
-			type: DataTypes.STRING(255),
-			allowNull: false,
-			unique: true
-		}
-	});
-};
+  sequelize.define('complaint', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    reportType: {
+      type: DataTypes.ENUM('Orden', 'Post', 'Usuario'),
+      allowNull: false,
+    },
+    complaint: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    evidence: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+  })
+}
--- api/src/models/Review.js
+++ api/src/models/Review.js
@@ -1,17 +1,20 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require('sequelize')
 
 module.exports = (sequelize) => {
-	sequelize.define('review', {
-		id: {
-			type: DataTypes.INTEGER,
-			autoIncrement: true,
-			allowNull: false,
-			primaryKey: true
-		},
-		title: {
-			type: DataTypes.STRING(255),
-			allowNull: false,
-			unique: true
-		}
-	});
-};
+  sequelize.define('review', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    summary: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    score: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  })
+}
