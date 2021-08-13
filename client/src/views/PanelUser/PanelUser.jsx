312af71dfc29085e2737d5447d1268100bc839dd
JAVIER KEVIN OJEDA <mambito1998@gmail.com>
2021-08-13 14:53:01 -0300

    Update PanelUser.jsx

--- client/src/views/PanelUser/PanelUser.jsx
+++ client/src/views/PanelUser/PanelUser.jsx
@@ -1,13 +1,37 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Footer from '../../Components/Footer/Footer'
 import Header from '../../Components/Header/Header'
 import UserData from '../../Components/PanelUser/UserData/UserData'
 
 function PanelUser() {
+    const { isAuthenticated } = useAuth0()
     return (
         <div>
             <Header />
-            <UserData />
+            {
+                isAuthenticated ?
+                    <UserData /> :
+                    <div className="NotFound">
+                        <div className="bg-white marginauto dark:bg-gray-800 h-full NotFoundContent min-h-screen">
+                            <div className="text-center w-full mx-auto   sm:px-6 lg:py-16 lg:px-8 z-20">
+                                <h2 className="text-3xl font-extrabold text-red-600 dark:text-white sm:text-4xl">
+                                    <span className="block text-red-600">No estas autorizado </span>
+                                </h2>
+                                <div className="lg:mt-0 lg:flex-shrink-0">
+                                    <div className="mt-5 inline-flex rounded-md">
+                                        <Link to="/home" className="m-5 ">
+                                            <button className="transition duration-500 ease-in-out px-10 py-2 transform hover:-translate-y-1 hover:scale-110 rounded-full text-white  bg-Viridian__Green font-bold hover:bg-Blue__Shappire ">
+                                                home
+                                            </button>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            }
             <Footer />
         </div>
     )
