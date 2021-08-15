259b970d8de7d3762fabb6304ef2645ac0fc400d
Manuel Miglioranza <mmiglioranza22@gmail.com>
2021-08-15 17:23:34 -0300

    add(route /failure agregada, para desarrollar)

--- api/src/routes/routers/checkout.js
+++ api/src/routes/routers/checkout.js
@@ -131,82 +131,81 @@ router.get('/success', async (req, res, next) => {
 
 	//let orderDB = await Order.findByPk(external_reference);
 
-	await Order.update(infoMP, {
-		where: {
-			id: external_reference
-		}
-	});
-	console.info('Guardando order en DB con datos de MP')
-	return res.redirect(`http://localhost:3000/paymentsuccesstest/${external_reference}`)
+	try {
+
+		await Order.update(infoMP, {
+			where: {
+				id: external_reference
+			}
+		});
+		console.info('Guardando order en DB con datos de MP')
+		return res.redirect(`http://localhost:3000/paymentsuccesstest/${external_reference}`)
+
+	} catch (err) {
+		next(err);
+	}
+
 });
 
 
+router.get('/failure', async (req, res, next) => {
 
-
-
-
-
-
-
-
-
-
-
+})
 
 //Ruta que recibe la información del pago
-router.get("/pagos", (req, res) => {              // no entiendo bien esta parte, a MP a buscar datos de pago??
-	console.info("EN LA RUTA PAGOS ", req)
-	const payment_id = req.query.payment_id
-	const payment_status = req.query.status
-	const external_reference = req.query.external_reference
-	const merchant_order_id = req.query.merchant_order_id
-	console.log("EXTERNAL REFERENCE ", external_reference)
+// router.get("/pagos", (req, res) => {              // no entiendo bien esta parte, a MP a buscar datos de pago??
+// 	console.info("EN LA RUTA PAGOS ", req)
+// 	const payment_id = req.query.payment_id
+// 	const payment_status = req.query.status
+// 	const external_reference = req.query.external_reference
+// 	const merchant_order_id = req.query.merchant_order_id
+// 	console.log("EXTERNAL REFERENCE ", external_reference)
 
-	//Aquí edito el status de mi orden --> pero en MP o en la DB??
-	Order.findByPk(external_reference)
-		.then((order) => {
-			order.payment_id = payment_id
-			order.payment_status = payment_status
-			order.merchant_order_id = merchant_order_id
-			order.status = "completed" // isComplete = true y status = 'approved'
-			console.info('Salvando order')
-			order.save()                // entiendo que esto tira un popup para guardar la order en la computadora del user. Si no, no entiendo que hace
-				.then((_) => {
-					console.info('redirect success')
+// 	//Aquí edito el status de mi orden --> pero en MP o en la DB??
+// 	Order.findByPk(external_reference)
+// 		.then((order) => {
+// 			order.payment_id = payment_id
+// 			order.payment_status = payment_status
+// 			order.merchant_order_id = merchant_order_id
+// 			order.status = "completed" // isComplete = true y status = 'approved'
+// 			console.info('Salvando order')
+// 			order.save()                // entiendo que esto tira un popup para guardar la order en la computadora del user. Si no, no entiendo que hace
+// 				.then((_) => {
+// 					console.info('redirect success')
 
-					return res.redirect("http://localhost:3000")
-				})
-				.catch((err) => {
-					console.error('error al salvar', err)
-					return res.redirect(`http://localhost:3000/?error=${err}&where=al+salvar`)
-				})
-		})
-		.catch(err => {
-			console.error('error al buscar', err)
-			return res.redirect(`http://localhost:3000/?error=${err}&where=al+buscar`)
-		})
+// 					return res.redirect("http://localhost:3000")
+// 				})
+// 				.catch((err) => {
+// 					console.error('error al salvar', err)
+// 					return res.redirect(`http://localhost:3000/?error=${err}&where=al+salvar`)
+// 				})
+// 		})
+// 		.catch(err => {
+// 			console.error('error al buscar', err)
+// 			return res.redirect(`http://localhost:3000/?error=${err}&where=al+buscar`)
+// 		})
 
-	//proceso los datos del pago 
-	//redirijo de nuevo a react con mensaje de exito, falla o pendiente
-})
+// 	//proceso los datos del pago 
+// 	//redirijo de nuevo a react con mensaje de exito, falla o pendiente
+// })
 
 
-//Busco información de una orden de pago
-router.get("/pagos/:id", (req, res) => {
-	const mp = new mercadopago(ACCESS_TOKEN)
-	const id = req.params.id
-	console.info("Buscando el id", id)
-	mp.get(`/v1/payments/search`, { 'status': 'pending' }) //{"external_reference":id})
-		.then(resultado => {
-			console.info('resultado', resultado)
-			res.json({ "resultado": resultado })
-		})
-		.catch(err => {
-			console.error('No se consulto:', err)
-			res.json({
-				error: err
-			})
-		})
-})
+// //Busco información de una orden de pago
+// router.get("/pagos/:id", (req, res) => {
+// 	const mp = new mercadopago(ACCESS_TOKEN)
+// 	const id = req.params.id
+// 	console.info("Buscando el id", id)
+// 	mp.get(`/v1/payments/search`, { 'status': 'pending' }) //{"external_reference":id})
+// 		.then(resultado => {
+// 			console.info('resultado', resultado)
+// 			res.json({ "resultado": resultado })
+// 		})
+// 		.catch(err => {
+// 			console.error('No se consulto:', err)
+// 			res.json({
+// 				error: err
+// 			})
+// 		})
+// })
 
 module.exports = router;
