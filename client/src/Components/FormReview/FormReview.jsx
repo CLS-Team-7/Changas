0050c7584191ba6e9997a65609828dca5d3a1f9e
Pablo Gomila <pablogiphone@icloud.com>
2021-08-15 19:35:07 -0300

    added(component form review) fixed (post demo images))

--- client/src/Components/FormReview/FormReview.jsx
+++ client/src/Components/FormReview/FormReview.jsx
@@ -1,6 +1,6 @@
 export default function AddFormReview(props) {
   return (
-    <div className="flex flex-col h-screen my-auto items-center  bg-cover">
+    <div className="flex flex-col my-auto items-center  bg-cover">
       <form className="content-center	w-2/4  border border-gray-800 bg-gray-200 border my-10	">
         <h1 className="text-2xl antialiased font-medium	">Deja una reseña para este servicio</h1>
         <div className="flex items-center border-b border-teal-500 py-2">
