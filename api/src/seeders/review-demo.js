d83bccc1066a583e913e98080e24fd8b3d6411f1
Manuel Miglioranza <mmiglioranza22@gmail.com>
2021-08-16 14:51:43 -0300

    order-demo seeders, corregidos. Falta armarlos bien

--- api/src/seeders/review-demo.js
+++ api/src/seeders/review-demo.js
@@ -7,47 +7,63 @@ const reviewsDB = [
         description: "Sarasa sarasa"
     },
     // {
-    //     review_id: 101,
+    //     id: 101,
+    //     user_id: 'e2f1cfbd-6c29-477d-b4eb-dbb69081dfd1',
+    //     post_id: "d671dc74-8c73-43db-b562-227367698c35",
     //     rating: 1,
     //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi"
     // },
     // {
-    //     review_id: 102,
+    //     id: 102,
+    //     user_id: 'e2f1cfbd-6c29-477d-b4eb-dbb69081dfd1',
+    //     post_id: "d671dc74-8c73-43db-b562-227367698c35",
     //     rating: 2,
     //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi"
     // },
     // {
-    //     review_id: 103,
+    //     id: 103,
+    //     user_id: 'e2f1cfbd-6c29-477d-b4eb-dbb69081dfd1',
+    //     post_id: "d671dc74-8c73-43db-b562-227367698c35",
     //     rating: 4,
     //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi"
     // },
     // {
-    //     review_id: 104,
+    //     id: 104,
+    //     user_id: 'e2f1cfbd-6c29-477d-b4eb-dbb69081dfd1',
+    //     post_id: "d671dc74-8c73-43db-b562-227367698c35",
     //     rating: 5,
     //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi"
     // },
     // {
-    //     review_id: 105,
+    //     id: 105,
+    //     user_id: 'e2f1cfbd-6c29-477d-b4eb-dbb69081dfd1',
+    //     post_id: "d671dc74-8c73-43db-b562-227367698c35",
     //     rating: 3,
     //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi"
     // },
     // {
-    //     review_id: 106,
+    //     id: 106,
+    //     user_id: 'e2f1cfbd-6c29-477d-b4eb-dbb69081dfd1',
+    //     post_id: "d671dc74-8c73-43db-b562-227367698c35",
     //     rating: 3,
     //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi"
     // },
     // {
-    //     review_id: 107,
+    //     id: 107,
     //     rating: 4,
     //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi"
     // },
     // {
-    //     review_id: 108,
+    //     id: 108,
+    //     user_id: 'e2f1cfbd-6c29-477d-b4eb-dbb69081dfd1',
+    //     post_id: "d671dc74-8c73-43db-b562-227367698c35",
     //     rating: 1,
     //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi"
     // },
     // {
-    //     review_id: 109,
+    //     id: 109,
+    //     user_id: 'e2f1cfbd-6c29-477d-b4eb-dbb69081dfd1',
+    //     post_id: "d671dc74-8c73-43db-b562-227367698c35",
     //     rating: 2,
     //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi"
     // },
