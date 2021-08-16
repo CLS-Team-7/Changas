8f799c976fb87b750455794ad784f51e2885b870
JAVIER KEVIN OJEDA <mambito1998@gmail.com>
2021-08-16 11:41:22 -0300

    borrando link a panel de admin y agregado el del home

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -64,7 +64,8 @@ function Header() {
     push(`/search/${title}`)
   }
 
-
+  const userLogin = useSelector(state => state.userLogin)
+  /* console.log(userLogin) */
   return (
     <div>
       <Disclosure as="nav" className="bg-gray-800">
@@ -75,7 +76,7 @@ function Header() {
                 <div className="flex items-center">
                   <div className="flex-shrink-0">
                     <Link
-                      to="/home"
+                      to="/"
                       className="flex title-font font-medium items-center  mb-4 md:mb-0"
                     >
                       <img
@@ -91,10 +92,10 @@ function Header() {
                       {
 
                         <Link
-                          to="/admin"
+                          to="/home"
                           className=" text-white px-3 py-2 rounded-md text-sm font-medium"
                         >
-                          Panel Admin
+                          Home
                         </Link>
                       }
                       <Link
@@ -223,6 +224,41 @@ function Header() {
                                     </Link>
                                   </div>
                                   <div className="flex flex-row justify-start items-center place-content-center">
+                                    {
+                                      userLogin?.isAdmin ?
+                                        <>
+                                          <div className="text-black">
+                                            <svg
+                                              xmlns="http://www.w3.org/2000/svg"
+                                              className="h-6 w-6"
+                                              fill="none"
+                                              viewBox="0 0 24 24"
+                                              stroke="currentColor"
+                                            >
+                                              <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                                              />
+                                              <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                              />
+                                            </svg>
+                                          </div>
+                                          <Link
+                                            to="/admin"
+                                            className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-300"
+                                          >
+                                            Admin
+                                          </Link></> :
+                                        <span></span>
+                                    }
+                                  </div>
+                                  <div className="flex flex-row justify-start items-center place-content-center">
                                     <svg
                                       xmlns="http://www.w3.org/2000/svg"
                                       className="h-6 w-6"
@@ -276,12 +312,6 @@ function Header() {
 
             <Disclosure.Panel className="md:hidden">
               <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                <Link
-                  to="/admin"
-                  className="  text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Panel Admin
-                </Link>
                 <Link
                   to="/faq"
                   className=" text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
