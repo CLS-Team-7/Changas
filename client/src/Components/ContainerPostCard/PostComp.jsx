61e8aa33f6c3df454dd7367460bc2ffbdab5af72
JAVIER KEVIN OJEDA <mambito1998@gmail.com>
2021-08-20 19:45:03 -0300

    eliminando los posts del menu de inicio

--- client/src/Components/ContainerPostCard/PostComp.jsx
+++ client/src/Components/ContainerPostCard/PostComp.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import CardPost from '../CardPost/CardPost'
+import CarruselHome from '../CarruselHome/CarruselHome'
 
 function PostComp({ posts }) {
-    if (posts.length === 0) {
+    if (posts?.length === 0) {
         return (
             <div className="container px-5 py-24 m-auto ">
                 <h1>No hay posts para esta categoría aún</h1>
@@ -10,18 +11,10 @@ function PostComp({ posts }) {
         )
     }
     return (
-        <div className=" container flex flex-wrap -m-4">
-            {posts.map(post => {
-                return <CardPost
-                    title={post.title}
-                    img={post.image}
-                    category={post.category}
-                    description={post.description}
-                    specialty={post.specialty}
-                    id={post.id}
-                    isPremium={post.isPremium}
-                    key={post.id} />
-            })}
+        <div className=" container flex flex-wrap -m-4 ">
+            <CarruselHome type={"Premium"} post={""} />
+            <CarruselHome type={"Ofertas de servicios"} post={""} />
+            <CarruselHome type={"Solicitudes de servicios"} post={""} />
         </div>
     )
 }
