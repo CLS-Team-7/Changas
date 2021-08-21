864ff3e26b23a440fc7d700f68a01d08a0c48a3b
Manuel Miglioranza <mmiglioranza22@gmail.com>
2021-08-20 22:53:51 -0300

    fixed(detalles inputs y forms)

--- client/src/Components/FormQuestion/FormQuestion.jsx
+++ client/src/Components/FormQuestion/FormQuestion.jsx
@@ -46,26 +46,22 @@ export default function AddFormQuestion(props) {
           <div class="py-3 max-w-md mx-auto">
             <div class="bg-white min-w-2xl flex flex-col rounded-xl shadow-lg">
               <div class="px-12 py-5">
-                <h2 class="text-gray-800 text-3xl font-semibold">Haz tu pregunta</h2>
+                <h2 class="text-gray-800 text-3xl font-semibold">¿Cuál es tu pregunta?</h2>
               </div>
               <div class="bg-blue-200 w-full flex flex-col items-center">
                 <div class="flex flex-col items-center py-6 space-y-3">
-                  {/*<div className="grid grid-cols-1">
-                    <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Nombre de Usuario</label>
-                    <input name="user_id" disabled value={userLogin.fullName} className="text-center py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" />
-                  </div>*/}
+                  <div className="grid grid-cols-1">
+                    <p className=" m-2 md:text-sm text-xs text-gray-900 text-light font-semibold text-center">
+                      El dueño de la publicación será notificado de tu pregunta y la misma se verá en el detalle de la publicación.</p>
+                    <p className=" m-2 md:text-sm text-xs text-gray-900 text-light font-semibold">
+                      Por favor, leé atentamente la publicación antes de preguntar algo que quizás ya tiene respuesta!
+                    </p>
+                  </div>
                   <div className="grid grid-cols-1">
                     <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Publicación</label>
                     <input name="post_id" disabled value={singlePost.title} className="text-center py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" />
                   </div>
-                  {/*<select onChange={handleChange} name='rating' className="w-50  border  hover:border-gray-500 px-4 py-2 pr-8 rounded  ">
-                    <option>Calificación</option>
-                    <option type='number' value='1'>1 ⭐</option>
-                    <option type='number' value="2">2 ⭐⭐</option>
-                    <option type='number' value="3">3 ⭐⭐⭐</option>
-                    <option type='number' value="4">4 ⭐⭐⭐⭐</option>
-                    <option type='number' value="5">5 ⭐⭐⭐⭐⭐</option>
-                  </select>*/}
+
                 </div>
                 <div class="w-3/4 flex flex-col">
                   <textarea rows="3" name="description" class="p-4 text-gray-500 rounded-xl resize-none" onChange={handleChange} autoComplete="off" placeholder="Tu pregunta aquí..." />
