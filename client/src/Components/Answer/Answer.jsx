da86cd8ad1511f497288991b621fa650aee8d6cf
Manuel Miglioranza <mmiglioranza22@gmail.com>
2021-08-21 20:42:04 -0300

    fixed(css de preguntas y respuestas)

--- client/src/Components/Answer/Answer.jsx
+++ client/src/Components/Answer/Answer.jsx
@@ -9,22 +9,21 @@ function Answer({ createdAt, answer }) {
 
     return (
         <div className="flex bg-white ">
-            <div className="">
-                <span className="flex items-center ml-2 text-black-600 text-xs">Respuesta</span>
-                <span className="flex items-center ml-2 text-black-600 text-xs">{createdAt}</span>
-
-                <div className="mt-3">
-                    <p className="mt-1 text-justify" >"{answer}"</p>
-                </div>
-                <div className="flex items-center ml-2">
-                    <div>
+            <div className="flex flex-col">
+                <p className="flex items-baseline">
+                    <span className="flex items-center text-black-600 text-xs">Respuesta</span>
+                    <span className="flex items-center ml-2 text-black-600 text-xs">{createdAt}</span>
+                    <span className="flex ml-auto">
                         {isAuthenticated ?
-                            <Link to={`/report`} className="flex pt-4 ml-auto"><button className="flex ml-auto text-white bg-blue-600 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Reportar</button></Link>
+                            <Link to={`/report`} className="flex pt-4"><span className="flex ml-auto text-gray  border-0 focus:outline-none hover:text-red-600 rounded text-xs">Reportar</span></Link>
                             : null}
-                    </div>
+                    </span>
+                </p>
+                <div className="mt-3">
+                    <p className="mt-1 ml-2 text-justify" >"{answer}"</p>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 export default Answer;
--- client/src/Components/Question/Question.jsx
+++ client/src/Components/Question/Question.jsx
@@ -12,27 +12,19 @@ function Question({ createdAt, question, given_name, id_question }) {
         <div>
 
             <div className="flex bg-white p-6  rounded-lg shadow-lg ml-6 border-2 mt-3 w-11/12">
-                <div className="ml-6 ">
+                <div className="flex flex-col ml-6">
                     <p className="flex items-baseline">
                         <span className="text-gray-600 font-bold">{given_name}</span>
                         <span className="ml-2 text-green-600 text-xs">Usuario Registrado</span>
+                        <span className="flex ml-auto">
+                            {isAuthenticated ?
+                                <Link to={`/report`} className="flex pt-4"><span className="flex ml-auto text-gray  border-0  focus:outline-none hover:text-red-600 rounded text-xs">Reportar</span></Link>
+                                : null}
+                        </span>
                     </p>
                     <span className="flex items-center ml-2 text-black-600 text-xs">{createdAt}</span>
-                    <div className="flex items-center mt-4 text-gray-600">
-                        <div className="flex items-center">
-                            <i className='commentsIcons'></i>
-                        </div>
-                    </div>
-                    <div className="mt-3">
-                        <p className="mt-1 text-justify" >"{question}"</p>
-                    </div>
-                    <div className="flex items-center ml-2">
-                        <div>
-                            {isAuthenticated ?
-                                <Link to={`/report`} className="flex pt-4 ml-auto"><button className="flex ml-auto text-white bg-blue-600 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Reportar</button></Link>
-                                : null}
-                        </div>
-                    </div>
+
+                    <p className="mt-2 ml-2 text-justify" >"{question}"</p>
 
                     <div className="mt-3">
                         <Answers id_question={id_question} />
