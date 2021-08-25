b8fdd09773d27dede5d2458c1d743fde1938111e
Manuel Miglioranza <mmiglioranza22@gmail.com>
2021-08-25 13:03:45 -0300

    fixed(error tipeo FAQ)

--- client/src/Components/FaqComponent/FaqComponent.jsx
+++ client/src/Components/FaqComponent/FaqComponent.jsx
@@ -200,7 +200,7 @@ function FaqComponen() {
             <div className="group-focus:max-h-screen max-h-0 bg-gray-200 px-4 overflow-hidden ease duration-500">
               <p className="p-2 text-lg	text-black antialiased text-justify">
                 Una vez registrados todos tus datos y logueado en la aplicación,
-                hacé click en la sección "Nueva Publicación", completá los campos según el tipo de publicación y listo!
+                hacé click en la sección "Publicar", completá los campos según el tipo de publicación y listo!
               </p>
               <p className="p-2 text-lg	text-black antialiased text-justify">
                 Podés <strong>promocionar </strong>tus publicaciones para tener
