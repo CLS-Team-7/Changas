df94fcc92db599aa0ffc2328ed3d62537a06a6b6
¨Mauro¨ <81174890+Mauro069@users.noreply.github.com>
2021-08-25 19:02:09 -0300

    Saque warnings

--- client/src/Components/FormQuestion/FormQuestion.jsx
+++ client/src/Components/FormQuestion/FormQuestion.jsx
@@ -1,44 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import { useHistory } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { postQuestion } from '../../Redux/actions' // aca va air la action postQuestion
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { postQuestion } from "../../Redux/actions"; // aca va air la action postQuestion
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 // import { useAuth0 } from "@auth0/auth0-react";
 
-export default function AddFormQuestion(props) { // falta chequear tambien que el mismo usuario no se haga preguntas a si mismo, ternario en el boton enviar
+export default function AddFormQuestion(props) {
+  // falta chequear tambien que el mismo usuario no se haga preguntas a si mismo, ternario en el boton enviar
   // const { user } = useAuth0();
   const dispatch = useDispatch();
-  const { push } = useHistory()
-  const singlePost = useSelector(state => state.singlePost)
-  const userLogin = useSelector(state => state.userLogin);
+  const { push } = useHistory();
+  const singlePost = useSelector((state) => state.singlePost);
+  const userLogin = useSelector((state) => state.userLogin);
 
   // console.log(user)
   //const user = useSelector(state => state.userAdmin);
   //const post = useSelector(state => state.postList);
-  console.log(userLogin)
+  console.log(userLogin);
 
   const [postInput, setPostInput] = useState({
     user_id: userLogin.id,
     post_id: singlePost.id,
     question: "",
-  })
+  });
   // useEffect(() => {
   //   dispatch(getUserAdmin())
   //   dispatch(getAllPosts())
   // }, [dispatch])
   function handleChange(e) {
-    setPostInput(values => ({
+    setPostInput((values) => ({
       ...values,
-      [e.target.name]: e.target.value
-    }))
+      [e.target.name]: e.target.value,
+    }));
   }
   const handleSubmit = (e) => {
-    e.preventDefault()
-    dispatch(postQuestion(postInput))
-    alert("¡Tu pregunta fue enviada! El dueño de la publicación fue notificado.")
-    push(`/posts/${singlePost.id}`) //La pregunta se va a mostrar, el usuario la tiene que responder desde su panel
-  }
+    e.preventDefault();
+    dispatch(postQuestion(postInput));
+    alert(
+      "¡Tu pregunta fue enviada! El dueño de la publicación fue notificado."
+    );
+    push(`/posts/${singlePost.id}`); //La pregunta se va a mostrar, el usuario la tiene que responder desde su panel
+  };
 
   return (
     <div>
@@ -48,29 +51,51 @@ export default function AddFormQuestion(props) { // falta chequear tambien que e
           <div className="p-3 max-w-md mx-auto">
             <div className="bg-white min-w-2xl flex flex-col rounded-xl shadow-lg">
               <div className="px-12 py-5">
-
-                <h2 className="text-gray-800 text-3xl font-semibold">¿Cuál es tu pregunta?</h2>
+                <h2 className="text-gray-800 text-3xl font-semibold">
+                  ¿Cuál es tu pregunta?
+                </h2>
               </div>
               <div className="bg-gray-700 w-full flex flex-col items-center rounded-b-xl">
                 <div className="flex flex-col items-center py-6 space-y-3 w-11/12 md:w-9/12">
                   <div className="grid grid-cols-1">
                     <p className=" m-2 md:text-sm text-xs text-gray-100 text-light font-semibold text-center">
-                      El dueño de la publicación será notificado de tu pregunta y la misma se verá en el detalle de la publicación.</p>
+                      El dueño de la publicación será notificado de tu pregunta
+                      y la misma se verá en el detalle de la publicación.
+                    </p>
                     <p className=" m-2 md:text-sm text-xs text-gray-100 text-light font-semibold">
-                      Por favor, leé atentamente la publicación antes de preguntar algo que quizás ya fue respondido allí.
+                      Por favor, leé atentamente la publicación antes de
+                      preguntar algo que quizás ya fue respondido allí.
                     </p>
                   </div>
                   <div className="grid grid-cols-1 w-11/12 md:w-9/12">
-                    <label className="uppercase md:text-sm text-xs text-gray-100 text-light font-semibold">- Publicación -</label>
-                    <input name="post_id" disabled value={singlePost.title} className="text-center text-gray-100 py-2 px-3 rounded-lg border-2 border-yellow-400 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" />
+                    <label className="uppercase md:text-sm text-xs text-gray-100 text-light font-semibold">
+                      - Publicación -
+                    </label>
+                    <input
+                      name="post_id"
+                      disabled
+                      value={singlePost.title}
+                      className="text-center text-gray-100 py-2 px-3 rounded-lg border-2 border-yellow-400 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                    />
                   </div>
-
                 </div>
                 <div className="w-11/12 md:w-9/12 flex flex-col">
+                  <textarea
+                    rows="3"
+                    name="question"
+                    className="p-4 text-gray-500 rounded-xl resize-none"
+                    onChange={handleChange}
+                    autoComplete="off"
+                    placeholder="Tu pregunta aquí..."
+                  />
 
-                  <textarea rows="3" name="question" className="p-4 text-gray-500 rounded-xl resize-none" onChange={handleChange} autoComplete="off" placeholder="Tu pregunta aquí..." />
-
-                  <button type='submit' onClick={(e) => handleSubmit(e)} className="py-3 my-8 text-lg rounded-xl text-white  bg-indigo-500 border-0 focus:outline-none hover:bg-indigo-600 rounded">Enviar</button>
+                  <button
+                    type="submit"
+                    onClick={(e) => handleSubmit(e)}
+                    className="py-3 my-8 text-lg rounded-xl text-white  bg-indigo-500 border-0 focus:outline-none hover:bg-indigo-600 rounded"
+                  >
+                    Enviar
+                  </button>
                 </div>
               </div>
             </div>
@@ -79,5 +104,5 @@ export default function AddFormQuestion(props) { // falta chequear tambien que e
       </form>
       <Footer />
     </div>
-  )
+  );
 }
--- client/src/Components/FormReview/FormReview.jsx
+++ client/src/Components/FormReview/FormReview.jsx
@@ -1,42 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import { useHistory } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { postReview } from '../../Redux/actions'
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { postReview } from "../../Redux/actions";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
-export default function AddFormReview(props) { /// chequear tambien que el mismo usuario no se haga preguntas a si mismo, ternario en el boton enviar
+export default function AddFormReview(props) {
+  /// chequear tambien que el mismo usuario no se haga preguntas a si mismo, ternario en el boton enviar
 
   const dispatch = useDispatch();
   const { push } = useHistory();
 
   //const user = useSelector(state => state.userAdmin);
   //const post = useSelector(state => state.postList);
-  const singlePost = useSelector(state => state.singlePost)
-  const userLogin = useSelector(state => state.userLogin);
+  const singlePost = useSelector((state) => state.singlePost);
+  const userLogin = useSelector((state) => state.userLogin);
 
   const [postInput, setPostInput] = useState({
     user_id: userLogin.id,
     post_id: singlePost.id,
     rating: null,
     description: "",
-  })
+  });
   // useEffect(() => {
   //   dispatch(getUserAdmin())
   //   dispatch(getAllPosts())
   // }, [dispatch])
   function handleChange(e) {
-    setPostInput(values => ({
+    setPostInput((values) => ({
       ...values,
-      [e.target.name]: e.target.value
-    }))
+      [e.target.name]: e.target.value,
+    }));
   }
   const handleSubmit = (e) => {
-    e.preventDefault()
-    dispatch(postReview(postInput))
-    alert("¡Tu reseña fue enviada! El dueño de la publicación fue notificado.")
-    push(`/posts/${singlePost.id}`) //OJO, antes de que se muestr el review, el dueño del posteo tiene que VALIDARLO
-  }
+    e.preventDefault();
+    dispatch(postReview(postInput));
+    alert("¡Tu reseña fue enviada! El dueño de la publicación fue notificado.");
+    push(`/posts/${singlePost.id}`); //OJO, antes de que se muestr el review, el dueño del posteo tiene que VALIDARLO
+  };
 
   return (
     <div>
@@ -46,33 +47,74 @@ export default function AddFormReview(props) { /// chequear tambien que el mismo
           <div className="p-3 max-w-md mx-auto ">
             <div className="bg-white min-w-2xl flex flex-col rounded-lg shadow-lg">
               <div className="px-12 py-5">
-                <h2 className="text-gray-800 text-3xl font-semibold">¡Gracias por dejar tu opinión!</h2>
+                <h2 className="text-gray-800 text-3xl font-semibold">
+                  ¡Gracias por dejar tu opinión!
+                </h2>
               </div>
               <div className="bg-gray-800 w-full flex flex-col items-center p-8 rounded-b-lg">
                 <div className="flex flex-col items-center py-6 space-y-3">
                   <div className="grid grid-cols-1">
                     <p className=" m-2 md:text-sm text-xs text-white text-light font-semibold text-center">
-                      Tu opinión nos importa porque ayuda a la comunidad de CHANGAS a encontrar servicios y trabajadores de excelencia</p>
+                      Tu opinión nos importa porque ayuda a la comunidad de
+                      CHANGAS a encontrar servicios y trabajadores de excelencia
+                    </p>
                     <p className=" m-2 md:text-sm text-xs text-gray-200 text-light font-semibold">
-                      ¡Con tu reseña estás ayudando al dueño de la publicación a encontrar más clientes!
+                      ¡Con tu reseña estás ayudando al dueño de la publicación a
+                      encontrar más clientes!
                     </p>
                   </div>
                   <div className="grid grid-cols-1 w-11/12 md:w-9/12 ">
-                    <label className="uppercase md:text-sm text-xs text-gray-200 text-light font-semibold">- Publicación -</label>
-                    <input name="post_id" disabled value={singlePost.title} className=" text-center text-gray-200 py-2 px-3 rounded-lg border-2 border-yellow-400 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" />
+                    <label className="uppercase md:text-sm text-xs text-gray-200 text-light font-semibold">
+                      - Publicación -
+                    </label>
+                    <input
+                      name="post_id"
+                      disabled
+                      value={singlePost.title}
+                      className=" text-center text-gray-200 py-2 px-3 rounded-lg border-2 border-yellow-400 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                    />
                   </div>
-                  <select onChange={handleChange} name='rating' className="w-11/12 md:w-9/12 border hover:border-gray-500 px-4 py-2 pr-8 rounded  ">
-                    <option disabled selected>¿Cómo calificarías tu experiencia?</option>
-                    <option type='number' value='1'>1 ⭐</option>
-                    <option type='number' value="2">2 ⭐⭐</option>
-                    <option type='number' value="3">3 ⭐⭐⭐</option>
-                    <option type='number' value="4">4 ⭐⭐⭐⭐</option>
-                    <option type='number' value="5">5 ⭐⭐⭐⭐⭐</option>
+                  <select
+                    onChange={handleChange}
+                    name="rating"
+                    className="w-11/12 md:w-9/12 border hover:border-gray-500 px-4 py-2 pr-8 rounded  "
+                  >
+                    <option disabled selected>
+                      ¿Cómo calificarías tu experiencia?
+                    </option>
+                    <option type="number" value="1">
+                      1 ⭐
+                    </option>
+                    <option type="number" value="2">
+                      2 ⭐⭐
+                    </option>
+                    <option type="number" value="3">
+                      3 ⭐⭐⭐
+                    </option>
+                    <option type="number" value="4">
+                      4 ⭐⭐⭐⭐
+                    </option>
+                    <option type="number" value="5">
+                      5 ⭐⭐⭐⭐⭐
+                    </option>
                   </select>
                 </div>
                 <div className="w-11/12 md:w-9/12  flex flex-col">
-                  <textarea rows="3" name="description" className="p-4 text-gray-500 rounded-xl resize-none" onChange={handleChange} autoComplete="off" placeholder="Contános tu experiencia " />
-                  <button type='submit' onClick={(e) => handleSubmit(e)} className="py-3 my-8 text-lg rounded-xl text-white  bg-indigo-500 border-0 focus:outline-none hover:bg-indigo-600 rounded">Enviar</button>
+                  <textarea
+                    rows="3"
+                    name="description"
+                    className="p-4 text-gray-500 rounded-xl resize-none"
+                    onChange={handleChange}
+                    autoComplete="off"
+                    placeholder="Contános tu experiencia "
+                  />
+                  <button
+                    type="submit"
+                    onClick={(e) => handleSubmit(e)}
+                    className="py-3 my-8 text-lg rounded-xl text-white  bg-indigo-500 border-0 focus:outline-none hover:bg-indigo-600 rounded"
+                  >
+                    Enviar
+                  </button>
                 </div>
               </div>
             </div>
@@ -81,5 +123,5 @@ export default function AddFormReview(props) { /// chequear tambien que el mismo
       </form>
       <Footer />
     </div>
-  )
+  );
 }
