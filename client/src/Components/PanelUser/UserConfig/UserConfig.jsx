4b7e9643646c41d4bd7be0fcd3639b60b504cfb8
Dario Gomez <dario_07_11@hotmail.com>
2021-08-26 22:56:57 -0300

    removed foto extra label

--- client/src/Components/PanelUser/UserConfig/UserConfig.jsx
+++ client/src/Components/PanelUser/UserConfig/UserConfig.jsx
@@ -81,7 +81,7 @@ function handleDeleteUser () {
                               {errors.age && touched.age && <div className="text-red-500 font-bold">{errors.age}</div>}
                               <div>
                                 <Field
-                                  className="text-center border bg-gray-100 py-2 px-4 w-3/4 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
+                                  className="border bg-gray-100 py-2 px-4 w-3/4 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
                                   name="age"
                                   validate={validateAge}
                                   autoComplete="off"
@@ -146,10 +146,7 @@ function handleDeleteUser () {
                             </div>
                           </div>
                           <div>
-                            <label className="mt-3  text-gray-700 font-bold inline-block mb-2">
-                              {" "}
-                              Foto / Imágen
-                            </label>
+                            
                             {errors.summary && touched.phoneNumber && (
                               <div className="text-red-500 font-bold">{errors.summary}</div>
                             )}
